c5e5c1d1f790c930d0e9eb13a039d186bce06884
AsmaaAboElHassan <a.muhammed3601@gmail.com>
2023-08-28 08:21:31 +0300

    create material app with text calss

---
+++ task4/flutter/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Text(
+      "Hello, How are you?",
+      textAlign: TextAlign.center,
+      style: TextStyle(color: Colors.lightGreen, fontWeight: FontWeight.bold , fontSize: 25),
+    )
+  ));
+}
